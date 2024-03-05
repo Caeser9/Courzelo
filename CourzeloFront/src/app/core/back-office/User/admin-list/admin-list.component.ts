f6d885e79db5e93b6c8c52e8a60f3d3e765b7634
youssek-kchaou <youssek.kchaou@esprit.tn>
2024-03-05 01:20:20 +0100

    list Admin&formateur

--- CourzeloFront/src/app/core/back-office/User/admin-list/admin-list.component.ts
+++ CourzeloFront/src/app/core/back-office/User/admin-list/admin-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProfileService } from 'src/app/service/profile.service';
 import { UserService } from 'src/app/service/user.service';
 import { Profile } from 'src/app/shared/model/profile.module';
@@ -11,51 +12,82 @@ import { User } from 'src/app/shared/model/user.model';
 })
 export class AdminListComponent implements OnInit {
   usersWithProfiles: any[] = [];
-  admin : any
- 
-  profile:any
+  admin: any
+  profileForm: FormGroup;
+  profile: any;
   FormateurSearch: string = '';
   filteredFormateur: User[] = [];
   formateur: any
-  constructor(private userService:UserService,private profileservice:ProfileService){}
+  updateProfileDialog: boolean = false
+  id: any
+  constructor(private userService: UserService, private profileservice: ProfileService, private formBuilder: FormBuilder,) { }
 
 
-ngOnInit(): void {
-
-  this.getAdmins();
-}
-getAdmins(){
-  this.admin=new User();
-  this.admin=this.userService.getUser("ROLE_ADMIN").subscribe((data) => {
-    this.admin = data;
-    this.filteredFormateur = this.admin;
-  },
-  (error) => {
-    console.error("Erreur lors de la récupération des données :", error);
-  }
-);
-
-}
-
-onSearch(): void {
-  console.log('Search Input:', this.FormateurSearch);
-
-  this.filteredFormateur = this.admin.filter(user =>
-    user.email.toLowerCase().includes(this.FormateurSearch.toLowerCase()) 
-  );
-  console.log('All admins:', this.admin);
-} 
-
-getProfileByIdUser(id: number) {
-  this.profileservice.getProfileByIdUser(this.admin.id).subscribe(
-    (data) => {
-      this.profile = data
+  ngOnInit(): void {
+    this.getAdmins();
+    this.profileForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      phone: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8), Validators.pattern(/^-?(0|[1-9]\d*){8}$/)]],
+      address: ['', [Validators.required, Validators.minLength(8)]],
 
     });
+  }
+  getAdmins() {
+    this.admin = new User();
+    this.admin = this.userService.getUser("ROLE_ADMIN").subscribe((data) => {
+      this.admin = data;
+      this.filteredFormateur = this.admin;
+    },
+      (error) => {
+        console.error("Erreur lors de la récupération des données :", error);
+      }
+    );
 
-}
-getProfilePhotoUrl(p: Profile): string {
-  return this.profileservice.getPhoto(p.photo);
-}
+  }
+
+  onSearch(): void {
+    console.log('Search Input:', this.FormateurSearch);
+
+    this.filteredFormateur = this.admin.filter(user =>
+      user.email.toLowerCase().includes(this.FormateurSearch.toLowerCase())
+    );
+    console.log('All admins:', this.admin);
+  }
+  getUserById(id: number) {
+
+    this.userService.getUserById(id).subscribe(
+      (data) => {
+        this.formateur = data
+
+      });
+  }
+  getProfileByIdUser(id: number) {
+    this.profileservice.getProfileByIdUser(id).subscribe(
+      (data) => {
+        this.profile = data
+        this.id = this.profile.id
+
+      });
+
+  }
+
+  updateProfile() {
+
+    this.profileservice.modifyProfile(this.id, this.profile).toPromise().then(
+      () => {
+        this.updateProfileDialog = false
+        console.log("Enseignant modifier avec succes")
+        this.getAdmins()
+      },
+      (error) => {
+        console.log("Problème servunu lors de la modification de l'enseignant")
+
+      }
+    )
+  }
+  getProfilePhotoUrl(p: Profile): string {
+    return this.profileservice.getPhoto(p.photo);
+  }
 
 }
